024c93863ea7fb6cb2e3779d8c770838840b5782
Keyur Dangasiya <keyurpatel3337@gmail.com>
2026-05-17 11:10:06 +0530

    Remove unnecessary temporary and sample files

--- check_tables.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new PDO('mysql:host=localhost;dbname=counselling;charset=utf8', 'root', 'GCA_Secure_#2026_Portal');
-$stmt = $conn->query('SHOW TABLES LIKE "%payment%"');
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-$stmt = $conn->query('SHOW TABLES LIKE "%fee%"');
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
--- test_detailed.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/common/helpers/report_functions.php';
-
-$mobile = '7990965567';
-$result = sendDetailedCollectionSummary($conn, $mobile);
-
-echo "Result: " . json_encode($result);
