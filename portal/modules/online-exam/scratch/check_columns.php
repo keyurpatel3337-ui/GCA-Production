1d3aa85fcea345561dd7d2939fa718eb5a733772
Keyur Dangasiya <keyurpatel3337@gmail.com>
2026-05-19 22:27:17 +0530

    chore: remove deprecated unused codebase files

--- portal/modules/online-exam/scratch/check_columns.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'c:/xampp/htdocs/GCA-Production/common/constants.php';
-require_once ENV_CONFIG_FILE;
-require_once DB_CONNECT_FILE;
-
-$stmt = $conn->query("SHOW COLUMNS FROM tbl_oes_questions");
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
